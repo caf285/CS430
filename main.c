95057322b4270fff4e543495364c6f40c2767f07
caf285 <caf285@nau.edu>
2016-09-12 01:01:51 -0700

    Update main.c
    Ability to read in a PPM, and stream contents to a new PPM

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    
+    FILE *fh = fopen("face.ppm", "rw");
+    if (fh == NULL) {
+        fprintf(stderr, "Error: File doesn't exist.\n");
+    }
+    int c = fgetc(fh);
+    if (c != 'P') {
+        fprintf(stderr, "Error: This is not a PPM file.\n");
+    }
+    
+    FILE *newFH = fopen("face2.ppm", "a+");
+    
+    while (c != EOF){
+        fputc(c, newFH);
+        printf("%c", c);
+        c = fgetc(fh);
+    }
+    fclose(fh);
     return 0;
 }
