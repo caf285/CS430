b0c0e012c04604a154c2c961e3564065dab21327
caf285 <caf285@nau.edu>
2016-09-15 10:47:36 -0700

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 // ================================================================================(read and built header)
 void ppmHeader(FILE *fh, int c, char *writeBuffer){
     
-    *writeBuffer++ = c;
+    *writeBuffer++ = c;                             // save output format in headerBuffer
     
-    while (1){
+    while (1){                                      // save comments to header
         c = fgetc(fh);
         *writeBuffer++ = c;
-        if (c == 10){
+        if (c == 10){                               // if new line doesn't start with comment, BREAK
             c = fgetc(fh);
             *writeBuffer++ = c;
             if (c != 35){
@@ -17,34 +17,36 @@ void ppmHeader(FILE *fh, int c, char *writeBuffer){
             }
         }
     }
-    int spaceTrigger = 0;
+    
+    int spaceTrigger = 0;                           // save x and y to headerBuffer
     while (1){
-        if (c == 32){
+        if (c == 32){                               // if space detected, set trigger to read rest of line
             spaceTrigger = 1;
         }
         c = fgetc(fh);
         *writeBuffer++ = c;
-        if (c == 10 && spaceTrigger == 1){
+        if (c == 10 && spaceTrigger == 1){          // if x and y on same line, BREAK
             break;
         }
-        else if (c == 10 && spaceTrigger == 0){
+        else if (c == 10 && spaceTrigger == 0){     // if x and y on seperate lines, set trigger for second loop
             spaceTrigger = 1;
         }
     }
-    while (1){
+    
+    while (1){                                      // save alpha to headerBuffer
         c = fgetc(fh);
         *writeBuffer++ = c;
         if (c == 10){
             break;
         }
     }
-    *writeBuffer = '\0';
+    *writeBuffer = '\0';                            // set end of headerBuffer
 }
 
 // ================================================================================(convert file to P3)
 void bufferP3(FILE *fh, char *writeBuffer, int inputNumber){
-    if (inputNumber == 51){              // convert P3 to P3
-        while (!feof(fh)){
+    if (inputNumber == 51){             // convert P3 to P3
+        while (!feof(fh)){              // simply copy file to buffer
             *writeBuffer++ = fgetc(fh);
         }
         *writeBuffer = EOF;
@@ -54,7 +56,7 @@ void bufferP3(FILE *fh, char *writeBuffer, int inputNumber){
             int total = 0;
             int c;
             c = fgetc(fh);
-            for (int i = 0; i < 8; i++) {
+            for (int i = 0; i < 8; i++) {           // convert binary data to number
                 if (!!((c << i) & 0x80)){
                     total += multiplier;
                 }
@@ -62,67 +64,53 @@ void bufferP3(FILE *fh, char *writeBuffer, int inputNumber){
             }
             multiplier = 100;
             int zeroTrigger = 0;
-            for(int i = 0; i < 3; i++){
+            for(int i = 0; i < 3; i++){             // finds single digits and copies them to buffer
                 int digit = total / multiplier;
                 total -= multiplier * digit;
                 multiplier = multiplier / 10;
-                if (digit > 0){
+                if (digit > 0){                     // sets trigger if first digit not 0
                     zeroTrigger = 1;
                 }
-                if (zeroTrigger == 1){
+                if (zeroTrigger == 1){              // if not 0, write number to buffer
                     *writeBuffer++ = (char) (digit+48);
-                }
+                }                                   // if entire number = 0, then just write 0 to buffer
                 if (i == 2 && zeroTrigger == 0){
                     *writeBuffer++ = (char) (48);
                 }
             }
-            *writeBuffer++  = (char) 10;
+            *writeBuffer++  = (char) 10;            // write line return to buffer
         }
-        *writeBuffer = EOF;
+        *writeBuffer = EOF;                         // write end of file to buffer
     }
 }
 
 // ================================================================================(convert file to P6)
 void bufferP6(FILE *fh, unsigned char *writeBuffer, int inputNumber){
-    if (inputNumber == 54){              // convert P6 to P6
-        while (!feof(fh)){
+    if (inputNumber == 54){                             // convert P6 to P6
+        while (!feof(fh)){              // copy file to buffer
             *writeBuffer++ = fgetc(fh);
         }
-        for (int i = 0; i <= 200; i+=50){
+        for (int i = 0; i <= 200; i+=50){ // add code at end of buffer to end when reading later
             *writeBuffer++ = i;
         }
-    }else{                              // convert from P3 to P6
+    }else{                                              // convert from P3 to P6
         while (1){
-            char *intBuffer1 = malloc(sizeof(char)*3);
+            char *intBuffer1 = malloc(sizeof(char)*3);  // buffer used to read single line, and reset for next line
             char *intBuffer2 = intBuffer1;
             int c = fgetc(fh);
             if (c == EOF)
                 break;
-            while (c != 10){
+            while (c != 10){            // read entire line; if line return detected, BREAK
                 *intBuffer2 = c;
-                
                 intBuffer2++;
                 c = fgetc(fh);
             }
-            *writeBuffer++ = atoi(intBuffer1);
+            *writeBuffer++ = atoi(intBuffer1); // copy number to dataBuffer
         }
-        for (int i = 0; i <= 200; i+=50){
+        for (int i = 0; i <= 200; i+=50){   // add code at end of buffer to end when reading later
             *writeBuffer++ = i;
         }
     }
-}
-
-// ================================================================================(combine header and data to build file)
-void buildFile(char *headerBuffer, char *dataBuffer, char *outputFile){
-    FILE *newFH = fopen(outputFile, "w+");
-    
-    while(*headerBuffer){
-        fputc(*headerBuffer++, newFH);
-    }
-    while(*dataBuffer != EOF){
-        fputc(*dataBuffer++, newFH);
-    }
-    fclose(newFH);
 }
 
 // ================================================================================(main)
@@ -132,58 +120,62 @@ int main(int argc, const char * argv[]) {
     FILE *fh = fopen(argv[1], "rw");                            // open file
     char *headerBuffer = malloc(sizeof(char)*50);               // create header buffer
     
-    if (fh == NULL) {                                           // check if file exists
+    if (fh == NULL) {                                           // ERROR if file doesn't exists
         fprintf(stderr, "Error: File doesn't exist.\n");
         return 1;
     }
     
     int c = fgetc(fh);                                          // read first character
-    if (c != 80) {                                              // check if first char is 'P'
+    if (c != 80) {                                              // ERROR if first char is not 'P'
         fprintf(stderr, "Error: This is not a PPM file.\n");
         return 1;
     }
     
-    char *writeBuffer;
+    char *writeBuffer;                                          // create node to traverse headerBuffer
     writeBuffer = headerBuffer;
     *writeBuffer++ = c;                                         // assign 'P' to header
     c = fgetc(fh);                                              // get next character
-    int inputNumber = c;
-    c = atoi(argv[3]) + 48;
+    int inputNumber = c;                                        // save input file type to check conversion requirement
+    c = atoi(argv[3]) + 48;                                     // save output file type to add to headerBuffer
     
     // ==================================================(build header)
-    ppmHeader(fh, c, writeBuffer);
+    ppmHeader(fh, c, writeBuffer);                              // build header (file path, output format, headerBuffer)
     
-    // ==================================================(build data)
+    // ==================================================(build P3 data)
     
-    if (atoi(argv[3]) == 3){
-        char *dataBuffer = malloc(sizeof(char)*10000000);
+    if (atoi(argv[3]) == 3){                                        // convert to P3
+        char *dataBuffer = malloc(sizeof(char)*10000000);   // create buffer for data
         writeBuffer = dataBuffer;
-        bufferP3(fh, writeBuffer, inputNumber);
         
+        bufferP3(fh, writeBuffer, inputNumber);             // build dataBuffer with P3 data
         
-        FILE *newFH = fopen(argv[2], "w+");
-        
-        while(*headerBuffer){
+        FILE *newFH = fopen(argv[2], "w+");                 // open output file
+        while(*headerBuffer){                               // write header to file
+            printf("%c", *headerBuffer);
             fputc(*headerBuffer++, newFH);
         }
-        while(*dataBuffer != EOF){
+        while(*dataBuffer != EOF){                          // write data to file
+            printf("%c", *dataBuffer);
             fputc(*dataBuffer++, newFH);
         }
-        fclose(newFH);
+        fclose(newFH);                                      // close output file
         
+        // ==============================================(build P3 data)
         
-    }else if (atoi(argv[3]) == 6){
-        unsigned char *dataBuffer = malloc(sizeof(char)*10000000);
+    }else if (atoi(argv[3]) == 6){                                  // convert to P6
+        unsigned char *dataBuffer = malloc(sizeof(char)*10000000);  // create data buffer
         unsigned char *writeBuffer2 = dataBuffer;
-        bufferP6(fh, writeBuffer2, inputNumber);
         
-        FILE *newFH = fopen(argv[2], "w+");
+        bufferP6(fh, writeBuffer2, inputNumber);            // build dataBuffer with P6 data
         
-        while(*headerBuffer){
+        FILE *newFH = fopen(argv[2], "w+");                 // open output file
+        while(*headerBuffer){                               // write header to file
+            printf("%c", *headerBuffer);
             fputc(*headerBuffer++, newFH);
         }
         int count = 0;
-        while(1){
+        while(1){                                           // write data to file
+            printf("%c", *dataBuffer);
             fputc(*dataBuffer++, newFH);
             if(*dataBuffer == count){
                 count += 50;
@@ -193,14 +185,13 @@ int main(int argc, const char * argv[]) {
             if (count == 200)
                 break;
         }
-        fclose(newFH);
+        fclose(newFH);                                      // close output file
         
-        
-    }else{
+    }else{                                                      // ERROR if wrong output format chosen
         fprintf(stderr, "Error: wrong PPM format.\n");
         return 1;
     }
     
-    fclose(fh);
+    fclose(fh);                                                 // close file
     return 0;
 }
